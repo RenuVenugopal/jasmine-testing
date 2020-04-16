400a528887ee1f66f7c98598132ea1638822608b
RenuVenugopal <renukv21@gmail.com>
2020-04-16 21:50:02 +0000

    adding more testing

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,11 @@
-function addition() {
-    return 42;
+function addition(numOne, numTwo) {
+    if(typeof(numOne) == "number" && typeof(numTwo) == "number")
+    {
+    return numOne + numTwo;
+    }
+    else
+    {
+    //return "Error!";
+    alert("Error!");
+    }
 }
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,7 +1,16 @@
-describe(Calculator, function() {
+describe("Calculator", function() {
     describe("Addition Tests", function() {
         it("should return 42", function() {
             expect(addition(22, 20)).toBe(42);
+        });
+        it("should return 26", function() {
+            expect(addition(17,9)).toBe(26);
+        });
+        it("should return an error if we don't supply two numbers", function() {
+            //expect(addition("Hitchhikers", "Guide")).toBe("Error!");
+            spyOn(window, "alert");
+            addition("Hitchhikkers", "Guide");
+            expect(window.alert).toHaveBeenCalledWith("Error!");
         });
     });
 });
