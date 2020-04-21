6b8474f9d880a3931c2d109d29021da855bd91d5
RenuVenugopal <renukv21@gmail.com>
2020-04-21 18:55:00 +0000

    adding script for the first challenge of testing of age

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,4 +1,4 @@
-function addition(numOne, numTwo) {
+/*function addition(numOne, numTwo) {
     if(typeof(numOne) == "number" && typeof(numTwo) == "number")
     {
     return numOne + numTwo;
@@ -8,4 +8,37 @@ function addition(numOne, numTwo) {
     //return "Error!";
     alert("Error!");
     }
+
+}*/
+
+/*Calculator = function() {
+    this.value = 0;
 }
+Calculator.prototype.add = function(number) {
+    if(typeof(number) == "number"){
+        this.value += number;
+        } else{
+            alert("Error!");
+        }
+}*/
+whatCanIDrink = function(age){
+    if (age<0){
+        return "Sorry. I can’t tell what drink because that age is incorrect!";
+    }
+    else if(age<14){
+        return "Drink Toddy";
+    }
+    else if(age>=14 && age<18){
+        return "Drink Coke";
+    }
+    else if(age<=18 && age>21){
+        return "Drink Beer";
+    }
+    else if(age<130){
+        return "Drink Whisky";
+    }
+    else{
+        return "Sorry. I can’t tell what drink because that age is incorrect!";
+    }
+
+};
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,7 +1,47 @@
-describe("Calculator", function() {
+/*describe("Calculator", function() {
     describe("Addition Tests", function() {
         it("should return 42", function() {
             expect(addition(22, 20)).toBe(42);
+        });
+        it("should return 26", function() {
+            expect(addition(17,9)).toBe(26);
+        });
+        it("should return an error if we don't supply two numbers", function() {
+            //expect(addition("Hitchhikers", "Guide")).toBe("Error!");
+            spyOn(window, "alert");
+            addition("Hitchhikkers", "Guide");
+            expect(window.alert).toHaveBeenCalledWith("Error!");
+        });
+    });
+});*/
+/*describe("Calculator", function() {
+    var calc = new Calculator;
+    //to clear the error occur last time we are using this built in function in jasmine
+    beforeEach(function() {
+        calc = new Calculator;
+    });
+    describe("Addition Tests", function() {
+        it("should return 42", function() {
+            calc.add(22);
+            calc.add(20);
+            expect(calc.value).toBe(42);
+        });
+        it("should return 26", function() {
+            calc.add(17);
+            calc.add(9);
+            expect(calc.value).toBe(26);
+        });
+        it("should return error if we don't supply numbers", function(){
+            spyOn(window, "alert");
+            calc.add("Hitchhikkers");
+            expect(window.alert).toHaveBeenCalledWith("Error!");
+        });
+    });
+});*/
+describe("DrinkAbout", function() {
+    describe("Appropriate drink Tests", function() {
+        it("Sorry. I can’t tell what drink because that age is incorrect!", function() {
+            expect(whatCanIDrink(22, 20)).toBe(42);
         });
         it("should return 26", function() {
             expect(addition(17,9)).toBe(26);
